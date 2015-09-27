9b21b26422138247dc1203e2c2f2a9ac4835905e
pgajbhiye <pallavi.mp88@gmail.com>
2015-09-27 22:26:11 +0530

    pointing to heroku

--- app/src/main/java/com/droid/floatboat/collabcart/config/Config.java
+++ app/src/main/java/com/droid/floatboat/collabcart/config/Config.java
@@ -4,7 +4,7 @@ package com.droid.floatboat.collabcart.config;
 
 public class Config{
 
-    public static final String SERVER_URL = "http://10.0.0.2:3000";
+    public static final String SERVER_URL = "https://collabcart.herokuapp.com";
 
     public static final int DEFAULT_CATEGORY = 74415;
 
